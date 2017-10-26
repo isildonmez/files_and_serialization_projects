9a00e891b2931e09b634b71381b31256acd9cefc
Isil Donmez <isil.donmez@gmail.com>
2017-10-26 15:43:55 +0100

    Change act method, Finish analyse_the_guess method, Finish how_many_guesses_remain

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,6 +1,5 @@
 class Hangman
   def initialize()
-    @secret_word = ""
     @mistaken_letters = []
 
     welcoming
@@ -18,9 +17,9 @@ class Hangman
   # TODO: the type of the secret_word
   def create_secret_code
     dictionary = File.readlines("5desk.txt")
-    sample_word = dictionary.sample.chomp
+    sample_word = dictionary.sample.strip
     until sample_word.length.between?(5, 12)
-      sample_word = dictionary.sample.chomp
+      sample_word = dictionary.sample.strips
     end
     @secret_word = sample_word.downcase
     p @secret_word
@@ -28,29 +27,37 @@ class Hangman
 
   def get_the_guess
     puts "Make a guest:"
-    guess = gets.chomp.downcase
-    # all_letters = guess.ord.between?(97, 122)
-    # one_letter = guess[1].nil?
-    # old_guess = @mistaken_letters.include? guess
+    @guess = gets.chomp.downcase
+    # all_letters = @guess.ord.between?(97, 122)
+    # one_letter = @guess[1].nil?
+    # old_@guess = @mistaken_letters.include? @guess
 
-    until guess.ord.between?(97, 122) && guess[1].nil? && !(@mistaken_letters.include? guess)
-      puts "Please make a new and valid guess"
-      guess = gets.chomp.downcase
+    until @guess.ord.between?(97, 122) &&
+          @guess[1].nil? &&
+          !(@mistaken_letters.include? @guess)
+      puts "Please make a new and valid @guess"
+      @guess = gets.chomp.downcase
     end
-    guess
+    @guess
   end
 
-  def print_wrong_guesses
+  def analyse_the_guess
+  	if @secret_word.include? @guess
+  	  visualise_the_word
+  	else
+  	  draw_a_hangman
+      @mistaken_letters << @guess
+      how_many_guesses_remain
+  	end
   end
 
-  def print_how_many_guesses_remain
-  end
-
-  def compare_the_guess
+  def how_many_guesses_remain
+  	@remained_guesses = 5 - @mistaken_letters.size
   end
 
   def visualise_the_word
-  	compare_the_guess
+
+  	@visualised_word = "visualised the word test"
   end
 
   def draw_a_hangman
@@ -61,19 +68,23 @@ class Hangman
 	#  |  |
 	# / \ |
 	#   __|_
+	@drawn_hangman = "drew the hangman test"
   end
 
   def act
   	  create_secret_code
   	  puts "#{@secret_word.length.times {print "- "}}"
-  	while @mistaken_letters.size < 7
+  	while @mistaken_letters.size < 6 ||
+          @visualised_word != @secret_word
       get_the_guess
-      draw_a_hangman
-      visualise_the_word
-      print_wrong_guesses
-      print_how_many_guesses_remain
-      @mistaken_letters = 7
+      analyse_the_guess
+      puts @drawn_hangman
+      puts @visualised_word
+      puts "Wrong guesses so far: #{@mistaken_letters.join(", ")}"
+      puts "#{@remained_guesses} guesses remain"
     end
+    puts "Game over" if @mistaken_letters.size == 6
+    puts "Congratulations" if @visualised_word == @secret_word
   end
 
 end
