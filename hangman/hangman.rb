5bc87c299b5afb5be6927d76667627d7251b8eea
Isil Donmez <isil.donmez@gmail.com>
2017-10-26 01:02:16 +0100

    Build the main structure of the whole methods and class

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,32 +1,64 @@
 class Hangman
   def initialize()
     @secret_word = ""
-    create_secret_code
+    @mistaken_letters = []
+
+    welcoming
+    draw_a_hangman
+    act
+  end
+
+  def welcoming
   end
 
   def create_secret_code
     dictionary = File.readlines("5desk.txt")
-    # p dictionary
     sample_word = dictionary.sample
-    puts sample_word
     until sample_word.length.between?(5, 12)
       sample_word = dictionary.sample
-      puts sample_word
     end
     @secret_word = sample_word
-    puts @secret_word
+  end
+
+  def get_the_guess
+  end
+
+  def print_wrong_guesses
+  end
+
+  def print_how_many_guesses_remain
+  end
+
+  def compare_the_guess
+  end
+
+  def visualise_the_word
+  	compare_the_guess
+  end
+
+  def draw_a_hangman
+	#  ___
+	#  | \|
+	#  O  |
+	# /|\ |
+	#  |  |
+	# / \ |
+	#   __|_
+  end
+
+  def act
+  	while @mistaken_letters.size < 7
+      create_secret_code
+      get_the_guess
+      draw_a_hangman
+      visualise_the_word
+      print_wrong_guesses
+      print_how_many_guesses_remain
+    end
   end
 
 end
 
 game = Hangman.new
-
-#  ___
-#  | \|
-#  O  |
-# /|\ |
-#  |  |
-# / \ |
-#   __|_
 
 
